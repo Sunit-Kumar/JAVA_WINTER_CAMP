750cb7debb666dd848d128ceea75c8cbc349386b
Sunit Kumar <96632346+Sunit-Kumar@users.noreply.github.com>
2022-12-20 10:59:25 +0530

    Library Management System

---
+++ Assignment2/src/LibraryManagement/student.java
@@ -0,0 +1,27 @@
+package LibraryManagement;
+
+import java.util.Scanner;
+
+public class student {
+
+    String studentName;
+    String regNum;
+
+    book borrowedBooks[] = new book[3];
+    public int booksCount = 0;
+
+    Scanner input = new Scanner(System.in);
+
+    // Constructor
+    public student()
+    {
+        // Print statement
+        System.out.println("Enter Student Name:");
+
+        // This keywords refers to current instance
+        this.studentName = input.nextLine();
+
+        System.out.println("Enter Registration Number:");
+        this.regNum = input.nextLine();
+    }
+}
